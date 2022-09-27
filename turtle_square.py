a2ccba5284995e9ab4ef787a0382f28870a23b81
Kaito <alexanderpuentes2004@gmail.com>
2022-09-27 18:38:40 -0500

    Construcción CRUD en python

---
+++ turtle_square.py
@@ -0,0 +1,22 @@
+import turtle
+tortuga = turtle.Turtle()
+window = turtle.Screen()
+
+def run():
+    tortuga.forward(100)
+    tortuga.right(90)
+
+    tortuga.forward(100)
+    tortuga.right(90)
+
+    tortuga.forward(100)
+    tortuga.right(90)
+
+    tortuga.forward(100)
+    tortuga.right(90)
+    
+    window.mainloop()
+    
+
+if __name__ == '__main__':
+    run()
